b08c6caade603158e61ab8ee29527a395c41e43b
Roy Sadaka <roy.sadaka@toluna.com>
2023-07-16 20:19:34 +0300

    v0.9.5

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ classifiers=[
 
 setup(
     name='venture-ai',
-    version='0.9.4',
+    version='0.9.5',
     description='A Fast, Flexible Trainer with Callbacks and Extensions for PyTorch',
     long_description_content_type='text/markdown',
     long_description=README_md,
